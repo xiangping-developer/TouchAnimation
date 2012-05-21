95c4ff12a9ec7468dac63694018989a9117e128e
Xiangping Meng <mengxiangpingx@gmail.com>
2012-05-21 09:53:33 +0800

    create project

---
+++ TouchAnimation/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TouchAnimation
+//
+//  Created by Meng Xiangping on 5/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
